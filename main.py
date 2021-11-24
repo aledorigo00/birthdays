8ce073688443716fc314ca57ab75eefaaa490652
Fabio Toffolo <fabietto@MacBook-Pro-di-Fabio-3.local>
2021-11-24 12:51:51 +0100

    fix codice

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import sys
 
 
 if len(sys.argv) > 1:
-    to_print = return_birthday(sys.argv[1])
+    my_argv= sys.argv[1]+" "+sys.argv[2]
+    to_print = return_birthday(my_argv)
 else:
     print('You have to rinput a name')
     exit()
 
 print("The name of this program is {}".format(sys.argv[0]))
-print("The birthday of {} is {}".format(sys.argv[1],to_print))
+print("The birthday of {} is {}".format(my_argv,to_print))
 
 
 
