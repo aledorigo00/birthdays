05aed854df668f8d535be6f0fc9909f710c87bc7
Alessandro Dorigo <alessandrodorigo@MacBook-Pro.local>
2021-11-24 12:47:40 +0100

    added birthday day

--- birthdays.py
+++ birthdays.py
@@ -1,11 +1,11 @@
 birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955',
+    'AlbertEinstein': '03/14/1879',
+    'BenjaminFranklin': '01/17/1706',
+    'AdaLovelace': '12/10/1815',
+    'DonaldTrump': '06/14/1946',
+    'RowanAtkinson': '01/6/1955',
     'FabioToffolo': '21/05/2000',
-    'Alan Turing': '23/07/1890'}
+    'AlanTuring': '23/07/1890'}
 
 def print_birthdays():
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
@@ -14,9 +14,27 @@ def print_birthdays():
 
 def return_birthday(name):
     if name in birthdays:
-        return birthdays[name]
+        date = birthdays[name]
+        return date
     else:
         print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+
+def birthday_month(name):
+    if name in birthdays:
+        date = birthdays[name]
+        date_elem = date.split('/')
+        return date_elem[1]
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+
+def birthday_day(name):
+    if name in birthdays:
+        date = birthdays[name]
+        date_elem = date.split('/')
+        return date_elem[0]
+    else:
+        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
+
 
 #commento
 #marco
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 #! /usr/bin/env python3
 from birthdays import return_birthday
-import sys
+from birthdays import birthday_month
+from birthdays import birthday_day
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("name", help="Insert a name to retrieve its birthday")
+parser.add_argument("--month","-m",action="store_true", help="Input this flag to obtain only the month")
+parser.add_argument("--day","-d",action="store_true", help="Input this flag to obtain only the day")
+args = parser.parse_args()
 
 
-if len(sys.argv) > 1:
-    to_print = return_birthday(sys.argv[1])
-else:
-    print('You have to rinput a name')
-    exit()
-
-print("The name of this program is {}".format(sys.argv[0]))
-print("The birthday of {} is {}".format(sys.argv[1],to_print))
-
+if args.month:
+	to_print = birthday_month(args.name)
+	print("The birthday month of {} is {}".format(args.name,to_print))
+elif args.day:
+	to_print = birthday_day(args.name)
+	print("The birthday day of {} is {}".format(args.name,to_print))
+else:	
+	to_print = return_birthday(args.name)
+	print("The birthday  of {} is {}".format(args.name,to_print))
 
 
